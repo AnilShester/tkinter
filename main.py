8de671191dfdfb8275f64851c2f505c3149a3745
Anil Shrestha <Anilshrestha645@gmail.com>
2024-08-30 19:09:23 +0300

    creating a window.

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Checking connection with github")
+from tkinter import *
+# creating a root window from as an instance of TK class of tkinter module
+root = Tk()
+
+root.title("Tk Example")
+root.configure(background="yellow")
+root.minsize(200, 200) #width and height
+root.maxsize(500, 500)
+root.geometry("300x300+50+50")
+root.mainloop()
